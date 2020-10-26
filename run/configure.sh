e1b033e0405fd47ed35cfb728bf1531309d8aa96
ramses <slonik256@yandex.ru>
2020-10-26 17:52:38 +0300

    version 0.2.2

---
+++ run/configure.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python ./run/service_configuration.pyc
---
+++ scripts/service_configuration.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import time
+import tqdm
+
+configuration_number = "0xFFA04A"
+version = "0.2.2"
+
+print("Service package 2: ver. {}".format(version))
+time.sleep(0.5)
+print("Service package 2: start configuration ")
+time.sleep(0.5)
+print("Service package 2: stage 1")
+time.sleep(0.5)
+for i in tqdm.tqdm(range(1000), ascii=True, desc="Finding dependencies"):
+    time.sleep(0.01)
+print("Service package 2: stage 1 finished")
+time.sleep(0.5)
+print("Service package 2: stage 2")
+time.sleep(1.5)
+print("Service package 2: configuration package 1 found")
+for i in tqdm.tqdm(range(5000), ascii=True, desc="Setup in progress"):
+    time.sleep(0.002)
+print("Service package 2: stage 2 finished")
+time.sleep(0.5)
+print("Service package 2: successfully configured!")
+time.sleep(0.5)
+print("Service package 2: configuration checksum : {}".format(configuration_number))
