0fd6bb69c268c56d17b2a91893430a89238b5586
ramses <slonik256@yandex.ru>
2020-10-28 19:44:51 +0300

    version 0.2.6

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -3,8 +3,8 @@
 import time
 import tqdm
 
-configuration_number = "0xFF02CB"
-version = "0.2.5"
+configuration_number = "0xFA02"
+version = "0.2.6"
 
 print("Service package 2: ver. {}".format(version))
 time.sleep(0.5)
